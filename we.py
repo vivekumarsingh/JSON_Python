70e3802251bab7ff9c92386494ccdfd7e6ad40ed
vivekumarsingh <46886866+vivekumarsingh@users.noreply.github.com>
2019-01-31 18:51:01 +0530

    Add files via upload
    add get and post to hit the link of api of survey , missions using json

---
+++ we.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+data ={"email": "anaand@tache.in", "password": "tachee"}
+data= requests.post("http://13.233.186.205/rest-auth/login/", data)
+headers = {'Content-type': 'application/json', 'Authorization': 'Token df60cad8f46658cdc1a03938aafdacebbef44f9c'}
+data = requests.get("http://13.233.186.205/links/missions/", headers=headers)          
+print(data.status_code)
+if data.status_code != 200:
+    print("Bad Status")
+elif data.status_code !=400:
+    print("OK Status") 
+a= data.json()
+print(data.text)
